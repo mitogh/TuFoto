72df6c13c94fd7a919e841c90920a6455cfae6a6
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-10-28 19:05:43 -0600

    Remove the setting menu from code

--- app/src/main/java/mitogh/com/github/tufoto/UI/Main.java
+++ app/src/main/java/mitogh/com/github/tufoto/UI/Main.java
@@ -61,9 +61,6 @@ public class Main extends ActionBarActivity{
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
